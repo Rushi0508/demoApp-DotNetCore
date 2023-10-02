baea0a73206ce17c54ee312c80734d8f9837312c
Rushi_0508 <rushigandhi14@gmail.com>
2023-10-02 18:26:55 +0530

    databse setup

---
+++ demoApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using demoApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace demoApp.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
